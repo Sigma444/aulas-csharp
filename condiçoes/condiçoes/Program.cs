a4a2bde979e010ffaaf6609bda8333bb2e903c04
moises silva <mcastilho125@gmail.com>
2022-12-01 11:18:35 -0300

    aulas csharp

---
+++ condiçoes/condiçoes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace condiçoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira sua idade");
+            int idade = int.Parse(Console.ReadLine());
+
+            if (idade > 18)
+            {
+                Console.WriteLine("É maior de idade");
+            }
+            else
+            {
+                Console.WriteLine("É menor de idade");
+            }
+            
+        }
+    }
+}
